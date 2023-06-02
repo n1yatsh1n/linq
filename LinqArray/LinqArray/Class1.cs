b98157a6480238ca27c9ae020c691f11ce9295d0
n1yatsh1n <116572958+n1yatsh1n@users.noreply.github.com>
2023-06-02 17:23:18 +0600

    Add files via upload

---
+++ LinqArray/LinqArray/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class program
+{
+    public static void Main()
+    {
+        int[] array = { 1, 0, 0, -545, 3, -4, 12, 105, 0, 2, 12 };
+        var posNums = from n in array
+            where n > 0
+            select n;
+        var negNums = from n in array
+            where n < 0
+            select n;
+        var notnullNums = from n in array
+            where n != 0
+            select n;
+        var evenNums = from n in array
+            where n % 2 == 0
+            select n;
+        int pr = 1;
+        foreach (int num in notnullNums)
+        {
+            pr *= num;
+        }
+        Console.WriteLine($"Кол-во положительных {posNums.Count()}");
+        Console.WriteLine($"Кол-во отрицательных {negNums.Count()}");
+        Console.WriteLine($"Произведение ненулевых {pr}");
+        Console.WriteLine($"Сумма чётных {evenNums.Sum()}");
+    }
+}
---
+++ LinqMinEl/LinqMinEl/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace LinqMinEl
+{
+    public class Class1
+    {
+
+        public static void Main()
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[n];
+            Console.WriteLine(n/2);
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            List<int> firstHalf = new List<int>(); 
+            List<int> secondHalf = new List<int>();
+            
+            for (int k = 0; k < n; k++)
+            {
+                if (k<n/2) firstHalf.Add(nums[k]);
+                if (k>n/2) secondHalf.Add(nums[k]);
+            }
+
+            var firstHalfSorted = from el in firstHalf orderby el select el;
+            var secondHalfSorted = (from el in secondHalf orderby el select el).Reverse();
+            Console.WriteLine("ОТВЕТ: ");
+            foreach (var firstEl in firstHalfSorted)
+            {
+                Console.WriteLine(firstEl);
+            }
+
+            foreach (var secondEl in secondHalfSorted)
+            {
+                Console.WriteLine(secondEl);
+            }
+        }
+    }
+}
